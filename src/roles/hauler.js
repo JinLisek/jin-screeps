389229ea07738de5f2636ca73bc53c4e3c2c5fb4
JinLisek <tastyramen@gmail.com>
2020-04-11 22:11:56 +0200

    Added bool in memory for hauler to check if hauling

--- src/roles/hauler.js
+++ src/roles/hauler.js
@@ -1,13 +1,13 @@
 var hauler = {
   run: function (creep) {
-    if (creep.store.getFreeCapacity() > 0) {
-      const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES);
-      if (creep.pickup(droppedEnergy[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(droppedEnergy[0], {
-          visualizePathStyle: { stroke: "#ffaa00" },
-        });
-      }
-    } else {
+    if (creep.memory.hauling && creep.store[RESOURCE_ENERGY] == 0) {
+      creep.memory.hauling = false;
+    }
+    if (!creep.memory.hauling && creep.store.getFreeCapacity() == 0) {
+      creep.memory.hauling = true;
+    }
+
+    if (creep.memory.hauling) {
       var targets = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
           return (
@@ -24,6 +24,13 @@ var hauler = {
             visualizePathStyle: { stroke: "#ffffff" },
           });
         }
+      }
+    } else {
+      const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES);
+      if (creep.pickup(droppedEnergy[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(droppedEnergy[0], {
+          visualizePathStyle: { stroke: "#ffaa00" },
+        });
       }
     }
   },
