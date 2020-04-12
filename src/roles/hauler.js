e75a1eadeca11d5a6b35185974ae384c57671442
JinLisek <tastyramen@gmail.com>
2020-04-12 21:52:25 +0200

    Added containers to haulers  search for energy

--- src/roles/hauler.js
+++ src/roles/hauler.js
@@ -27,11 +27,24 @@ const hauler = (creep) => {
       creep.moveTo(24, 32);
     }
   } else {
-    const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES);
-    if (creep.pickup(droppedEnergy[0]) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(droppedEnergy[0], {
-        visualizePathStyle: { stroke: "#ffaa00" },
-      });
+    const containers = creep.room.find(FIND_STRUCTURES, {
+      filter: (structure) =>
+        structure.structureType == STRUCTURE_CONTAINER &&
+        structure.store.getUsedCapacity(RESOURCE_ENERGY) > 101,
+    });
+    if (containers.length > 0) {
+      if (creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(containers[0], {
+          visualizePathStyle: { stroke: "#ffaa00" },
+        });
+      }
+    } else {
+      const droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES);
+      if (creep.pickup(droppedEnergy[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(droppedEnergy[0], {
+          visualizePathStyle: { stroke: "#ffaa00" },
+        });
+      }
     }
   }
 };
