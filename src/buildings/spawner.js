3a031bebb5d8feb8f5a2a2773f8f50826a5082a9
JinLisek <tastyramen@gmail.com>
2020-04-12 19:40:18 +0200

    Added homeRoom to creep's memory

--- src/buildings/spawner.js
+++ src/buildings/spawner.js
@@ -52,13 +52,15 @@ const spawner = {
     ];
 
     spawns.forEach((spawn) => {
-      for (var name in Memory.creeps) {
+      for (const name in Memory.creeps) {
         if (!Game.creeps[name]) {
           if (Memory.creeps[name]["role"] == "miner") {
             const sourceId = Memory.creeps[name]["source"];
-            Memory["rooms"][spawn.room.name]["sources"][sourceId][
-              "miner"
-            ] = null;
+            if (sourceId != undefined) {
+              Memory["rooms"][spawn.room.name]["sources"][sourceId][
+                "miner"
+              ] = null;
+            }
           }
 
           delete Memory.creeps[name];
@@ -66,45 +68,41 @@ const spawner = {
         }
       }
 
-      const numOfCreeps = Object.keys(Game.creeps).length;
-      if (numOfCreeps < 1) {
-        var newName = harvester_config["role"] + Game.time;
-        spawn.spawnCreep(harvester_config["parts"], newName, {
-          memory: { role: harvester_config["role"] },
-        });
-      } else {
-        for (let i = 0; i < unit_configs.length; ++i) {
-          const unit = unit_configs[i];
-          let num_of_units = _.filter(
-            Game.creeps,
-            (creep) =>
-              creep.memory.role == unit["role"] &&
-              creep.memory.homeRoom == spawn.room.name
-          ).length;
+      for (let i = 0; i < unit_configs.length; ++i) {
+        const unit = unit_configs[i];
+        let num_of_units = _.filter(
+          Game.creeps,
+          (creep) =>
+            creep.memory.role == unit["role"] &&
+            creep.memory.homeRoom == spawn.room.name
+        ).length;
 
-          if (num_of_units < unit["max_num"]) {
-            var bodyParts = unit["parts"];
-            while (costOfBody(bodyParts) > spawn.room.energyCapacityAvailable) {
-              bodyParts.pop();
-            }
-            if (costOfBody(unit["parts"]) <= spawn.room.energyAvailable) {
-              if (unit["role"] == "claimer") {
-                var newName = unit["role"] + Game.time;
-                const destinationRoom = num_of_units < 1 ? "E15N42" : "E16N41";
-                console.log("Spawning new " + newName);
-                spawn.spawnCreep(unit["parts"], newName, {
-                  memory: { role: unit["role"], destination: destinationRoom },
-                });
-              } else {
-                var newName = unit["role"] + Game.time;
-                console.log("Spawning new " + newName);
-                spawn.spawnCreep(unit["parts"], newName, {
-                  memory: { role: unit["role"] },
-                });
-              }
-            }
-            break;
+        if (num_of_units < unit["max_num"]) {
+          var bodyParts = unit["parts"];
+          while (costOfBody(bodyParts) > spawn.room.energyCapacityAvailable) {
+            bodyParts.pop();
           }
+          if (costOfBody(unit["parts"]) <= spawn.room.energyAvailable) {
+            if (unit["role"] == "claimer") {
+              var newName = unit["role"] + Game.time;
+              const destinationRoom = num_of_units < 1 ? "E15N42" : "E16N41";
+              console.log("Spawning new " + newName);
+              spawn.spawnCreep(unit["parts"], newName, {
+                memory: {
+                  role: unit["role"],
+                  destination: destinationRoom,
+                  homeRoom: spawn.room.name,
+                },
+              });
+            } else {
+              var newName = unit["role"] + Game.time;
+              console.log("Spawning new " + newName);
+              spawn.spawnCreep(unit["parts"], newName, {
+                memory: { role: unit["role"], homeRoom: spawn.room.name },
+              });
+            }
+          }
+          break;
         }
       }
 
