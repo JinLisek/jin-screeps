e17e616aa15fce3cfb7341920fcf7d749e9b7e13
JinLisek <tastyramen@gmail.com>
2020-04-12 12:34:12 +0200

    Improved claimer path finding - use findRoute

--- src/roles/claimer.js
+++ src/roles/claimer.js
@@ -1,17 +1,15 @@
 const claimer = {
   run: function (creep) {
-    console.log("CLAIMER in " + creep.room.name);
-    if (creep.room.name == "E15N41") {
-      const exit = creep.pos.findClosestByRange(FIND_EXIT_TOP);
+    const routeToReservableRoom = Game.map.findRoute(creep.room, "E15N42");
+    console.log("CLAIMER route length = " + routeToReservableRoom.length);
+    if (routeToReservableRoom.length > 0) {
+      const exit = creep.pos.findClosestByRange(routeToReservableRoom[0].exit);
       creep.moveTo(exit);
-    } else if (creep.room.name == "E15N42") {
+    } else {
       const controller = creep.room.controller;
       if (creep.reserveController(controller) == ERR_NOT_IN_RANGE) {
         creep.moveTo(controller);
       }
-    } else if (creep.room.name == "E14N42") {
-      const exit = creep.pos.findClosestByRange(FIND_EXIT_RIGHT);
-      creep.moveTo(exit);
     }
   },
 };
