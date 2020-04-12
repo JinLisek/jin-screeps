d31e336e884e5eb45197d42ded314e51fbae0428
JinLisek <tastyramen@gmail.com>
2020-04-12 23:37:23 +0200

    Some refactor to spawner - added shouldSpawn function to unitConfig

--- src/buildings/spawner.js
+++ src/buildings/spawner.js
@@ -1,33 +1,70 @@
+const costOfBody = (body) =>
+  body.reduce((cost, part) => cost + BODYPART_COST[part], 0);
+
+const calculateAvailableBodyInRoom = (body, room) => {
+  let bodyParts = body;
+  while (costOfBody(bodyParts) > room.energyCapacityAvailable) {
+    bodyParts.pop();
+  }
+  return bodyParts;
+};
+
+const defaultShouldSpawn = (room, unitConfig) => {
+  const numOfUnitsInRoom = _.filter(
+    Game.creeps,
+    (creep) =>
+      creep.memory.role == unitConfig["role"] &&
+      creep.memory.homeRoom == room.name
+  ).length;
+
+  return numOfUnitsInRoom < unitConfig["max_num"];
+};
+
 const unit_configs = [
   {
     parts: [MOVE, WORK, WORK, WORK, WORK, WORK],
     role: "miner",
     max_num: 2,
+    shouldSpawn: (room, unitConfig) => {
+      const numOfMinersInRoom = _.filter(
+        Game.creeps,
+        (creep) =>
+          creep.memory.role == unitConfig["role"] &&
+          creep.memory.homeRoom == room.name
+      ).length;
+
+      return numOfMinersInRoom < Object.keys(room.memory["sources"]).length;
+    },
   },
   {
     parts: [CARRY, CARRY, MOVE, MOVE],
     role: "hauler",
-    max_num: 2,
+    max_num: 3,
+    shouldSpawn: defaultShouldSpawn,
   },
   {
     parts: [WORK, WORK, CARRY, MOVE, CARRY, MOVE],
     role: "upgrader",
-    max_num: 4,
+    max_num: 2,
+    shouldSpawn: defaultShouldSpawn,
   },
   {
     parts: [WORK, WORK, CARRY, MOVE, CARRY, MOVE],
     role: "repairer",
     max_num: 2,
+    shouldSpawn: defaultShouldSpawn,
   },
   {
     parts: [WORK, WORK, CARRY, MOVE, CARRY, MOVE],
     role: "builder",
-    max_num: 4,
+    max_num: 2,
+    shouldSpawn: defaultShouldSpawn,
   },
   {
     parts: [CLAIM, MOVE],
     role: "claimer",
-    max_num: 2,
+    max_num: 4,
+    shouldSpawn: defaultShouldSpawn,
   },
 ];
 
@@ -35,12 +72,7 @@ const harvester_config = {
   parts: [WORK, WORK, CARRY, MOVE],
   role: "harvester",
   max_num: 1,
-};
-
-const costOfBody = (body) => {
-  return body.reduce(function (cost, part) {
-    return cost + BODYPART_COST[part];
-  }, 0);
+  shouldSpawn: defaultShouldSpawn,
 };
 
 const spawner = () => {
@@ -67,70 +99,46 @@ const spawner = () => {
   spawns.forEach((spawn) => {
     for (let i = 0; i < unit_configs.length; ++i) {
       const unit = unit_configs[i];
-      let num_of_units = _.filter(
-        Game.creeps,
-        (creep) =>
-          creep.memory.role == unit["role"] &&
-          creep.memory.homeRoom == spawn.room.name
-      ).length;
 
-      if (unit["role"] == "miner") {
-        if (num_of_units < spawn.room.find(FIND_SOURCES).length) {
-          let bodyParts = unit["parts"];
-          while (costOfBody(bodyParts) > spawn.room.energyCapacityAvailable) {
-            bodyParts.pop();
-          }
-          if (costOfBody(bodyParts) <= spawn.room.energyAvailable) {
+      if (unit["shouldSpawn"](spawn.room, unit)) {
+        const bodyParts = calculateAvailableBodyInRoom(
+          unit["parts"],
+          spawn.room
+        );
+        if (bodyParts.length < 1) {
+          break;
+        }
+        if (costOfBody(bodyParts) <= spawn.room.energyAvailable) {
+          if (unit["role"] == "claimer") {
+            const num_of_claimersForE15N42 = _.filter(
+              Game.creeps,
+              (creep) =>
+                creep.memory.role == unit["role"] &&
+                creep.memory.homeRoom == spawn.room.name &&
+                creep.memory.destination == "E15N42"
+            ).length;
+            const newName = unit["role"] + Game.time;
+            const destinationRoom =
+              num_of_claimersForE15N42 < 2 ? "E15N42" : "E16N41";
+            console.log(
+              spawn.room.name + ": spawning new " + newName + " " + bodyParts
+            );
+            spawn.spawnCreep(bodyParts, newName, {
+              memory: {
+                role: unit["role"],
+                destination: destinationRoom,
+                homeRoom: spawn.room.name,
+              },
+            });
+          } else {
             const newName = unit["role"] + Game.time;
             console.log(spawn.room.name + ": spawning new " + newName);
             spawn.spawnCreep(bodyParts, newName, {
               memory: { role: unit["role"], homeRoom: spawn.room.name },
             });
           }
-
-          break;
         }
-      } else {
-        if (num_of_units < unit["max_num"]) {
-          let bodyParts = unit["parts"];
-          while (costOfBody(bodyParts) > spawn.room.energyCapacityAvailable) {
-            bodyParts.pop();
-          }
-          if (bodyParts.length < 1) {
-            break;
-          }
-          if (costOfBody(bodyParts) <= spawn.room.energyAvailable) {
-            if (unit["role"] == "claimer") {
-              const num_of_claimersForE15N42 = _.filter(
-                Game.creeps,
-                (creep) =>
-                  creep.memory.role == unit["role"] &&
-                  creep.memory.homeRoom == spawn.room.name &&
-                  creep.memory.destination == "E15N42"
-              ).length;
-              const newName = unit["role"] + Game.time;
-              const destinationRoom =
-                num_of_claimersForE15N42 < 1 ? "E15N42" : "E16N41";
-              console.log(
-                spawn.room.name + ": spawning new " + newName + " " + bodyParts
-              );
-              spawn.spawnCreep(bodyParts, newName, {
-                memory: {
-                  role: unit["role"],
-                  destination: destinationRoom,
-                  homeRoom: spawn.room.name,
-                },
-              });
-            } else {
-              const newName = unit["role"] + Game.time;
-              console.log(spawn.room.name + ": spawning new " + newName);
-              spawn.spawnCreep(bodyParts, newName, {
-                memory: { role: unit["role"], homeRoom: spawn.room.name },
-              });
-            }
-          }
-          break;
-        }
+        break;
       }
     }
 
