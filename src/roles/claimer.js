f5acdd14196cb416df8e60f37b1bf699a9f45095
JinLisek <tastyramen@gmail.com>
2020-04-12 12:24:09 +0200

    Changed claimer to reserveController for now

--- src/roles/claimer.js
+++ src/roles/claimer.js
@@ -5,13 +5,13 @@ const claimer = {
       const exit = creep.pos.findClosestByRange(FIND_EXIT_TOP);
       creep.moveTo(exit);
     } else if (creep.room.name == "E15N42") {
-      const exit = creep.pos.findClosestByRange(FIND_EXIT_LEFT);
-      creep.moveTo(exit);
-    } else if (creep.room.name == "E14N42") {
       const controller = creep.room.controller;
-      if (creep.claimController(controller) == ERR_NOT_IN_RANGE) {
+      if (creep.reserveController(controller) == ERR_NOT_IN_RANGE) {
         creep.moveTo(controller);
       }
+    } else if (creep.room.name == "E14N42") {
+      const exit = creep.pos.findClosestByRange(FIND_EXIT_RIGHT);
+      creep.moveTo(exit);
     }
   },
 };
