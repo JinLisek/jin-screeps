32b2103f1c39d76ac96808ecc68bed0e5bcac51f
JinLisek <tastyramen@gmail.com>
2020-04-13 22:51:23 +0200

    Fixed repairer - now correctly check's buildings' hits percentage

--- src/roles/repairer.js
+++ src/roles/repairer.js
@@ -1,5 +1,23 @@
 const finders = require("roles_helpers_finders");
 
+const goRepair = (creep) => {
+  const repairTarget = Game.getObjectById(creep.memory.targetId);
+  if (repairTarget && repairTarget.hits < repairTarget.hitsMax) {
+    if (creep.repair(repairTarget) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(repairTarget, {
+        maxRooms: 1,
+        visualizePathStyle: { stroke: "#ffffff" },
+      });
+    }
+  } else {
+    delete creep.memory.targetId;
+  }
+};
+
+const differentiateHitsPercentage = (struct) =>
+  struct.structureType == STRUCTURE_RAMPART || struct.structureType == STRUCTURE_WALL
+    ? struct.hits / struct.hitsMax + 0.8
+    : struct.hits / struct.hitsMax;
 const repairer = (creep) => {
   if (creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
     creep.memory.repairing = false;
@@ -10,31 +28,18 @@ const repairer = (creep) => {
 
   if (creep.memory.repairing) {
     if (creep.memory.targetId) {
-      const repairTarget = Game.getObjectById(creep.memory.targetId);
-      if (repairTarget && repairTarget.hits < repairTarget.hitsMax) {
-        if (creep.repair(repairTarget) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(repairTarget, {
-            maxRooms: 1,
-            visualizePathStyle: { stroke: "#ffffff" },
-          });
-        }
-      } else {
-        delete creep.memory.targetId;
-      }
+      goRepair(creep);
     } else {
       const structsInNeedOfRepair = creep.room.find(FIND_STRUCTURES, {
         filter: (struct) => struct.hits < struct.hitsMax,
       });
 
-      _.sortBy(structsInNeedOfRepair, (struct) => {
-        return struct.structureType == STRUCTURE_WALL || struct.structureType == STRUCTURE_WALL
-          ? struct.hits / struct.hitsMax + 0.5
-          : struct.hits / struct.hitsMax;
-      });
+      const sortedStructsToRepair = _.sortBy(structsInNeedOfRepair, differentiateHitsPercentage);
 
-      const structToRepair = structsInNeedOfRepair[structsInNeedOfRepair.length - 1];
+      const structToRepair = sortedStructsToRepair[0];
       if (structToRepair) {
         creep.memory.targetId = structToRepair.id;
+        goRepair(creep);
       }
     }
   } else {
